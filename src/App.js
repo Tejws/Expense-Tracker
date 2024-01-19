d6b77cc9ecd3135e705294ba096abdc7f63e5ad7
Tejas Ingle <tejas.ingle@somaiya.edu>
2024-01-19 16:59:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { Income } from './components/Income';
+
+import {AddTransaction} from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      
+          <Header />
+          <div className='container'>
+          <Balance />
+          <Income/>
+          <TransactionList/>
+          <AddTransaction/>
+
+          </div>
+    </GlobalProvider>
   );
 }
 
